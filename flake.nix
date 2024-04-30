35713b61dac3a489a17d52beb07cda57700fc5ca
Josh Robson Chase <josh@robsonchase.com>
2024-04-30 19:16:21 -0400

    flake update

--- flake.nix
+++ flake.nix
@@ -1,92 +1,39 @@
 {
-  description = "A Rust project using naersk";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    # Note: fenix packages are cached via cachix:
-    #       cachix use nix-community
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, naersk, flake-utils, fenix }:
+  outputs = { self, utils, nixpkgs, ... }@inputs: utils.lib.eachDefaultSystem (system:
     let
-      # If you have a workspace and your binary isn't at the root of the
-      # repository, you may need to modify this path.
-      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-      name = cargoToml.package.name;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.fenix.overlays.default
+        ];
+      };
+      cargoWorkspace = pkgs.callPackage ./Cargo.nix {
+        buildRustCrateForPkgs = pkgs: with pkgs; buildRustCrate.override {
+          rustc = fenix.complete.rustc;
+          cargo = fenix.complete.cargo;
+        };
+      };
     in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            fenix.overlays.default
-          ];
-        };
-        components = [
-          "cargo"
-          "rustc"
+    {
+      inherit cargoWorkspace;
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [
+          cargoWorkspace.rootCrate.build
         ];
-        dev-components = components ++ [
-          "clippy"
-          "rustfmt"
-          "rust-src"
+        buildInputs = with pkgs; [
+          crate2nix
+          rust-analyzer-nightly
+          fenix.complete.clippy
+          fenix.complete.rustfmt
         ];
-        toolchain = pkgs.fenix.complete.withComponents components;
-        dev-toolchain = pkgs.fenix.complete.withComponents dev-components;
-        naersk-lib = naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-        fix-n-fmt = pkgs.writeShellScriptBin "fix-n-fmt" ''
-          set -euf -o pipefail
-          ${dev-toolchain}/bin/cargo clippy --fix --allow-staged --allow-dirty --all-targets --all-features
-          ${dev-toolchain}/bin/cargo fmt
-        '';
-        cargo-udeps = pkgs.writeShellScriptBin "cargo-udeps" ''
-          set -euf -o pipefail
-          PATH=${pkgs.cargo-udeps}/bin:$PATH
-          CARGO_TARGET_DIR=$(git rev-parse --show-toplevel)/target/udeps cargo-udeps "$@"
-        '';
-        defaultPackage = naersk-lib.buildPackage {
-          pname = name;
-          root = ./.;
-        };
-      in
-      rec {
-        inherit defaultPackage;
-
-        packages = builtins.listToAttrs [{ inherit name; value = defaultPackage; }];
-
-        # Update the `program` to match your binary's name.
-        defaultApp = {
-          type = "app";
-          program = "${defaultPackage}/bin/hello";
-        };
-
-        devShell = pkgs.mkShell {
-          inputsFrom = [
-            defaultPackage
-          ];
-          buildInputs = with pkgs; [
-            dev-toolchain
-            rust-analyzer
-            clang
-            lld
-            fix-n-fmt
-            cargo-udeps
-          ];
-          RUST_SRC_PATH = "${dev-toolchain}/lib/rustlib/src/rust/library";
-        };
-      }
-    );
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        RUST_BACKTRACE = "true";
+      };
+    });
 }
